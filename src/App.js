1c0a5e825eb45b97d91781a80298e65249c8b8b1
まお <120441901+pinchan1213@users.noreply.github.com>
2024-03-15 07:12:41 +0000

    stateのリフトアップ

--- src/App.js
+++ src/App.js
@@ -2,37 +2,41 @@
 // useStateは特別な関数でコンポーネントから呼び出すことで記憶する
 import { useState } from "react";
 
-function Square() {
-  // valueは現在の値を格納、setValueはその値を更新するために使う
-  // nullは初期値
-  const [value, setValue] = useState(null);
-  function handleClick() {
-    setValue("foo!");
-  }
+function Square({ value, onSquareClick }) {
   return (
-    <button className="square" onClick={handleClick}>
+    <button className="square" onClick={onSquareClick}>
       {value}
     </button>
   );
 }
 
 export default function Board() {
+  // 9個のnullを持つ配列を与えるようにする
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    // slice()配列メソッドを使いsquares配列のコピーを作成する
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+  }
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        {/* レンダーする各Squareにpropsとして値を渡す */}
+        <Square value={squares[0]} onSquareClick={handleClick} />
+        <Square value={squares[1]} onSquareClick={handleClick} />
+        <Square value={squares[2]} onSquareClick={handleClick} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} onSquareClick={handleClick} />
+        <Square value={squares[4]} onSquareClick={handleClick} />
+        <Square value={squares[5]} onSquareClick={handleClick} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} onSquareClick={handleClick} />
+        <Square value={squares[7]} onSquareClick={handleClick} />
+        <Square value={squares[8]} onSquareClick={handleClick} />
       </div>
     </>
   );
